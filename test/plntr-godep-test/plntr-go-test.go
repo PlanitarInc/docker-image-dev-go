ce2dc025b4a19cea13cee37ad9a258e6fe1a07cd
korya <korya@github.com>
2015-02-02 17:29:31 -0500

    Add a real outside dependancy
    The dependancy is `etcd`; this project should live long enough

--- test/plntr-godep-test/plntr-go-test.go
+++ test/plntr-godep-test/plntr-go-test.go
@@ -2,8 +2,10 @@ package main
 
 import (
 	plntrdep "github.com/PlanitarInc/docker-image-dev-go/test/plntr-dep"
+	"github.com/coreos/go-etcd/etcd"
 )
 
 func main() {
+	_ = &etcd.Client{}
 	plntrdep.PlntrDepTest()
 }
